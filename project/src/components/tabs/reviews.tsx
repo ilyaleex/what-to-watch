3ea336db3616f684871feef2bcab401ec30bd47d
ilyaleex <gooooodguy@mail.ru>
2022-07-26 20:27:55 +0300

    refactoring: take out constants

--- project/src/components/tabs/reviews.tsx
+++ project/src/components/tabs/reviews.tsx
@@ -1,11 +1,16 @@
 import {Comment, CommentProps} from '../../types/comment';
 import dayjs from 'dayjs';
 
+const COMMENTS_IN_FIRST_COL = 3;
+
 type CommentsProps = {
   comments: Comment[]
 }
 
 function Review({comment}: CommentProps): JSX.Element {
+  const DATE_TIME = dayjs(comment.date).format('YYYY-MM-DD');
+  const DATE_TIME_FOR_STRING = dayjs(comment.date).format('MMMM DD, YYYY');
+
   return (
     <div className="review">
       <blockquote className="review__quote">
@@ -13,8 +18,8 @@ function Review({comment}: CommentProps): JSX.Element {
 
         <footer className="review__details">
           <cite className="review__author">{comment.user.name}</cite>
-          <time className="review__date" dateTime={dayjs(comment.date).format('YYYY-MM-DD')}>
-            {dayjs(comment.date).format('MMMM DD, YYYY')}
+          <time className="review__date" dateTime={DATE_TIME}>
+            {DATE_TIME_FOR_STRING}
           </time>
         </footer>
       </blockquote>
@@ -28,7 +33,7 @@ function Reviews({comments}: CommentsProps): JSX.Element {
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {comments.slice(0, 3).map((comment) =>
+        {comments.slice(0, COMMENTS_IN_FIRST_COL).map((comment) =>
           (
             <Review
               key={comment.id}
@@ -38,7 +43,7 @@ function Reviews({comments}: CommentsProps): JSX.Element {
         )}
       </div>
       <div className="film-card__reviews-col">
-        {comments.slice(3, -2).map((comment) =>
+        {comments.slice(COMMENTS_IN_FIRST_COL).map((comment) =>
           (
             <Review
               key={comment.id}
